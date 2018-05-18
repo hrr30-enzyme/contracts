471123ae80a065e937e4d476301a3dfc01127a1f
William Cory <willcory10@gmail.com>
2018-05-17 22:46:01 -0700

    make each contract for only 1 question

--- AnswerFactory.sol
+++ AnswerFactory.sol
@@ -4,20 +4,28 @@ import "./QuestionFactory.sol";
 
 contract AnswerFactory is QuestionFactory {
 
-    event NewAnswer(uint questionId, uint answerId, address answerer, uint stake);
+    uint answerFee = .001 ether;
+
+    event NewAnswer(uint questionId, uint answerId, address answerer);
 
     struct Answer {
         uint questionId;
-        uint stake;
+        uint upvotes;
     }
 
     Answer[] public answers;
 
-    mapping (uint => address) public questionToOwner;
-    
-    function _createQuestion(uint _questionId) public payable {
-        uint id = answers.push(Answer(_questionId, msg.value)) - 1;
-        questionToOwner[id] = msg.sender;
-        NewAnswer(_questionId, id, msg.sender, msg.value);
+    mapping (uint => address) public answerToOwner;
+
+    function setAnswerFee(uint _fee) external onlyOwner {
+        answerFee = _fee;
     }
+
+    function _createQuestion(uint _questionId) public payable {
+        require(msg.value >= answerFee);
+        uint id = answers.push(Answer(_questionId, 0)) - 1;
+        answerToOwner[id] = msg.sender;
+        emit NewAnswer(_questionId, id, msg.sender);
+    }
+
 }
--- QuestionFactory.sol
+++ QuestionFactory.sol
@@ -2,24 +2,14 @@ pragma solidity ^0.4.23;
 
 import "./Ownable.sol";
 
-contract QuestionFactory is Ownable {
-
-    event NewQuestion(uint questionId, address asker, uint bounty);
+contract Question is Ownable {
 
     uint duration = 1 days;
+    uint bounty;
+    uint minBounty = .003 ether;
 
-    struct Question {
-        uint bounty;
-        uint32 endTime;
-    }
-
-    Question[] public questions;
-
-    mapping (uint => address) public questionToOwner;
-    
-    function _createQuestion() public payable {
-        uint id = questions.push(Question(msg.value, uint32(now + duration))) - 1;
-        questionToOwner[id] = msg.sender;
-        NewQuestion(id, msg.sender, msg.value);
+    constructor() {
+        require(msg.value >= minBounty);
+        bounty = msg.value;
     }
 }
