b996bedb69b188ca0edbddfde52c7386222d07f8
William Cory <willcory10@gmail.com>
2018-05-21 00:37:18 -0700

    run migrations again

--- migrations/2_2nd_migration.js
+++ migrations/2_2nd_migration.js
@@ -1,9 +1,9 @@
 // const AnswerFactory = artifacts.require('./AnswerFactory.sol');
 // const Question = artifacts.require('./Question.sol');
 // const QuestionFactory = artifacts.require('./QuestionFactory.sol');
-const QuestionInterface = artifacts.require('./QuestionFactory.sol');
+const UpVote = artifacts.require('./UpVote.sol');
 // const Upvote = artifacts.require('./Upvote.sol');
 
 module.exports = function(deployer) {
-  deployer.deploy(QuestionInterface);
+  deployer.deploy(UpVote);
 };
